c672e552cc05f904ab7a6fa4000cdedc36e8e28d
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-01 15:04:13 +0900

    index.html controller routes write

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'reports/index'
+  get 'reports/index' => "reports#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
