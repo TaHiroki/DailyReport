fb81c96c9172cb9a6f76662e0621925c8e875245
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-03 14:09:00 +0900

    add flash report,destroy

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -42,6 +42,7 @@ class ReportsController < ApplicationController
   def destroy
     @report = Report.find_by(id: params[:id])
     @report.destroy
+    flash[:notice] = "削除しました"
     redirect_to("/reports/index")
   end
 end
