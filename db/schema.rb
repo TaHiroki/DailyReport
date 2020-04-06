dfe3011a2aa3fc45868f812c42bacc04b8bbe8d9
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-06 14:35:34 +0900

    add Model likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_141807) do
+ActiveRecord::Schema.define(version: 2020_04_06_053449) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_04_04_141807) do
     t.datetime "updated_at", null: false
     t.integer "report_id"
     t.integer "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "report_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reports", force: :cascade do |t|
