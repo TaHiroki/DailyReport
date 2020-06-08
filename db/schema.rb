de878734590ceaee45402c3faa4353dd80372f62
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-06-08 20:08:47 +0900

    add Docker

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_06_123046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.datetime "created_at", null: false
